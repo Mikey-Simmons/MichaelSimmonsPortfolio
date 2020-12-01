c1e5bb82f520602dcf01cd947ee9099856af55d5
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2020-12-01 10:41:55 -0800

    added phone#

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import 'bootstrap/dist/css/bootstrap.css';
 import HomePage from './views/HomePage';
 import { Router } from '@reach/router';
 
+
 function App() {
   return (
     <div className="App">
       <div className="jumbotron">
         <h1 className="display-4">Michael Simmons</h1>
         <p class="lead">Full-Stack Web Developer</p>
+        <p class="lead"> <a href="tel:2064681060">206-468-1060</a></p>
         <ul class="nav justify-content-center">
           <li class="nav-item">
             <a class="nav-link active" href="/">Home</a>
@@ -25,7 +27,7 @@ function App() {
           </li>
         </ul>
       </div>
-
+      
 <Router>
 <HomePage path="/"></HomePage>
 </Router>
