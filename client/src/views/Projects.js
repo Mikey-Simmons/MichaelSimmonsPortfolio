b4f5e54bae974a80a76a51c7d8805915680a3cf2
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2020-12-02 01:30:13 -0800

    more style :)

--- client/src/views/Projects.js
+++ client/src/views/Projects.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Jumbotron, Button, ListGroup, ListGroupItem } from 'reactstrap';
 import { AnimationWrapper } from 'react-hover-animation';
 
+
 const Projects = (props) => {
     return(
 
@@ -13,6 +14,12 @@ const Projects = (props) => {
   <path fill-rule="evenodd" d="M11.251.068a.5.5 0 0 1 .227.58L9.677 6.5H13a.5.5 0 0 1 .364.843l-8 8.5a.5.5 0 0 1-.842-.49L6.323 9.5H3a.5.5 0 0 1-.364-.843l8-8.5a.5.5 0 0 1 .615-.09z"/>
 </svg> Projects</h1>
 </AnimationWrapper>
+<ListGroup>
+<AnimationWrapper><ListGroupItem className="listy" tag="a" href="http://ec2-18-218-231-53.us-east-2.compute.amazonaws.com/">Apartment Connect</ListGroupItem></AnimationWrapper>
+<AnimationWrapper><ListGroupItem className="listy" tag="a" href="#">4Sale</ListGroupItem></AnimationWrapper>
+<AnimationWrapper><ListGroupItem className="listy" tag="a" href="#">FlatScreenGuru</ListGroupItem></AnimationWrapper>
+<AnimationWrapper><ListGroupItem className="listy" tag="a" href="#">Alien Invasion</ListGroupItem></AnimationWrapper>
+    </ListGroup>
         </Jumbotron>
     </div>
     )
