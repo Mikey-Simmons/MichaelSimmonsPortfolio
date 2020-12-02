2f62cd7d501f1638470585715c0be7d1a19b1c72
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2020-12-01 17:24:45 -0800

    style

--- client/src/views/Contact.js
+++ client/src/views/Contact.js
@@ -5,11 +5,11 @@ const Contact = (props) => {
   return (
     <div>
       <Jumbotron>
-        <h1 className="display-3">Contact Me</h1>
+        <h1 className="display-3">Contact Me!</h1>
         
         <ListGroup flush>
-      <ListGroupItem className="listy"  tag="a" href="tel:+12064681060">(206)-468-1060</ListGroupItem>
-      <ListGroupItem className="listy" tag="a" href="mailto: mikeysimmons425@gmail.com">mikeysimmons425@gmail.com</ListGroupItem>
+      Phone Number: <ListGroupItem className="listy"  tag="a" href="tel:+12064681060">(206)-468-1060</ListGroupItem>
+     Email: <ListGroupItem className="listy" tag="a" href="mailto: mikeysimmons425@gmail.com">mikeysimmons425@gmail.com</ListGroupItem>
       <ListGroupItem className="listy" tag="a" href="https://www.linkedin.com/in/michael-simmons-23491a160/">LinkedIn</ListGroupItem>
       <ListGroupItem className="listy" tag="a" href="https://github.com/Mikey-Simmons">Github</ListGroupItem>
     </ListGroup>
